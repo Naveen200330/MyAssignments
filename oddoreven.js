1e859cba4d1e1dd938d089bb49add17fe6b4544d
Naveen200330 <amnaveen2003@gmail.com>
2026-09-01 00:07:23 +0530

    Add files via upload

---
+++ oddoreven.js
@@ -0,0 +1,13 @@
+function isOddOrEven (){
+    
+    let n=166
+
+    if (n%2==0){
+    console.log("The Given Number is Even")
+        }
+    else{
+     console.log("The Given Number is Odd")
+        }
+}
+
+isOddOrEven(); 
