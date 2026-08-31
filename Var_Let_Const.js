c18232fc4ff6e3848a58b0265e732a7499e20768
Naveen200330 <amnaveen2003@gmail.com>
2026-09-01 00:14:14 +0530

    Add files via upload

---
+++ Var_Let_Const.js
@@ -0,0 +1,20 @@
+const browserName='Chrome'
+
+function getBrowserName(){
+    if (browserName==='Chrome'){
+        var browserName='Firebox'
+         }
+        
+    console.log(browserName)
+
+
+}
+
+getBrowserName();   // It will Print undefined
+
+
+
+
+//If let and const is used it wiil print Chrome
+
+
